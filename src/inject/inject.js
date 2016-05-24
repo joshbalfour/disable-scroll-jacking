5e85896ddcb1ad7702ee45e2b7677e2d0b9f1853
Josh Balfour <josh@joshbalfour.co.uk>
2016-05-24 13:53:48 +0100

    add boilerplate
    💜 http://extensionizr.com

---
+++ src/inject/inject.js
@@ -0,0 +1,13 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		if (window.jQuery) {
+
+		}
+		
+
+	}
+	}, 10);
+});
